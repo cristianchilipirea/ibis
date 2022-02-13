bb6e619a30941d8dbf5ac02fe6584aa425b4a4a2
cristian.chilipirea <cristian.chilipirea@gmail.com>
2022-02-14 00:15:12 +0200

    spellcheck

--- frontend/www/admin-viewRezults.php
+++ frontend/www/admin-viewRezults.php
@@ -1,7 +1,7 @@
 <form action="" method="post" name="myForm">
 	<input type="text" name="username" placeholder="username" />
 	<input type="text" name="task" placeholder="task Name" />
-	<button>View Rezults</button>
+	<button>View Results</button>
 </form>
 
 <?php
